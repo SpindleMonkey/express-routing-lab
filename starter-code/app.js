b2c648246de96a6a4bd0f3395348f55162f82ae3
connie kephart <spindle.monkey@gmail.com>
2017-07-20 19:11:23 -0600

    adds a home page with api documentation

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,11 +14,19 @@ var candyRouter = require('./candyRouter.js');
  * Middleware
  */
 
+// make files available for reqs
+app.use(express.static('public'));
+
 // send requests through body-parser before sending them to the controller
 app.use(bodyParser.json());
 
 // send all /candies requests to the candyRouter
 app.use('/candies', candyRouter);
+
+// hey! let's be helpful and provide a little documentation
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 // start the server and tell it to listen on port 3000
 app.listen(3000);
